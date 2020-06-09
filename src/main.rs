b6e509fa208351c77c7299bbaf41f89a46bbac36
Christopher Klimas <caklimas@gmail.com>
2020-06-09 16:14:54 -0500

    Setup the window

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,17 @@ mod display;
 use ggez::*;
 
 fn main() {
-    let mut cpu = cpu::Cpu::new();
-    cpu.load_rom(vec![0x13, 0xC5]);
+    let mut c = cpu::Cpu::new();
+    c.load_rom(vec![0x13, 0xC5]);
 
-    let c = conf::Conf::new();
-    let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("Hello ggez", "caklimas@gmail.com")
-        .conf(c)
+    let mut conf = conf::Conf::new();
+    conf.window_setup = conf::WindowSetup::default().title("Chip8");
+    conf.window_mode = conf::WindowMode::default().dimensions(cpu::VIDEO_HEIGHT as f32, cpu::VIDEO_WIDTH as f32);
+
+    let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("Chip8", "caklimas@gmail.com")
+        .conf(conf)
         .build()
         .expect("Error building context");
 
-    event::run(ctx, event_loop, &mut cpu).expect("Error running loop");
+    event::run(ctx, event_loop, &mut c).expect("Error running loop");
 }
